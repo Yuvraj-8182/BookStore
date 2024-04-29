e3c809e46a92040e2f7a3c6e4393107f1128b60a
yuvraj <yuvrajgoswami8182@gmail.com>
2024-04-29 19:59:33 +0530

    version 2.2

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,16 +1,19 @@
 import React from 'react'
-import Navbar from './components/Navbar'
-import Banner from './components/Banner'
-import Footer from './components/Footer'
-import Freebook from './components/Freebook'
+import Home from './home/Home'
+import {Routes,Route} from 'react-router-dom'
+import Courses from "./course/Courses"
+
 
 const App = () => {
   return (
     <>
-    <Navbar/>
-   <Banner/>
-   <Freebook/>
-   <Footer/>
+
+    <div className='dark:bg-slate-900 dark:text-white'>
+    <Routes>
+            <Route path='/' element={<Home/>}/>
+            <Route path='/course' element={<Courses/>}/>
+    </Routes>
+    </div>
    </>
   )
 }
--- Frontend/src/components/Cards.jsx
+++ Frontend/src/components/Cards.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 function Cards({item}) {
   return (
     <>
-        <div>
-        <div className="card w-96 bg-base-100 shadow-xl">
+        <div className="mt-4 my-3 p-3">
+        <div className="card w-92 bg-base-100 shadow-xl hover:scale-105 duration-300 dark:bg-slate-900 dark:text-white dark:border">
   <figure><img src={item.image} alt="Shoes" /></figure>
   <div className="card-body">
     <h2 className="card-title">
@@ -14,7 +14,7 @@ function Cards({item}) {
     <p>{item.title}</p>
     <div className="card-actions justify-between">
       <div className="badge badge-outline">${item.price}</div> 
-      <div className="badge badge-outline hover:text-white bg-pink-500 duration-300">Buy Now</div>
+      <div className="px-2 py-1 rounded-full border-[2px] hover:text-white hover:bg-pink-500 duration-300 cursor-pointer">Buy Now</div>
     </div>
   </div>
 </div>
